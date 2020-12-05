2e8879ba34bd516d5df280eebbb70f88b5f4a041
pjpscriv <justabitdope+git@gmail.com>
2020-12-05 14:07:46 +1300

    day 4

---
+++ day_04_1.py
@@ -0,0 +1,37 @@
+from functools import reduce
+
+expected_fields = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid']
+
+def parse_passports(lines):
+    passports = []
+    pp = {}
+    for line in lines:
+        if line.strip() == "":
+            passports.append(pp)
+            pp = {}
+        else:
+            fields = line.split(' ')
+            for field in fields:
+                # print('F:', field)
+                key_pair = field.split(':')
+                key = key_pair[0].strip()
+                value = key_pair[1].strip()
+                pp[key] = value
+    return passports
+
+
+def is_valid(passport):
+    for f in expected_fields:
+        if not f in passport.keys():
+            return 0
+    return 1
+
+file = open('input/day_04_1.txt')
+lines = file.readlines()
+file.close()
+
+passports = parse_passports(lines)
+
+valid = reduce(lambda valid_count, pp: valid_count + is_valid(pp), passports, 0)
+
+print(valid)
