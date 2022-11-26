c1dcbaf5d5d00f861f1341b883cefd2e7940b27f
pjpscriv <justabitdope+git@gmail.com>
2022-11-26 14:29:06 +1300

    day 19.2

--- day_19/part_2.py
+++ day_19/part_2.py
@@ -1,8 +1,11 @@
 from email import message
+from functools import reduce
+from math import ceil
 
 LOOP_LIMIT = 2
 
-node_map = {}
+rule_map = {}
+result_map = {}
 
 class Rule:
     id = None
@@ -17,9 +20,17 @@ class Rule:
         else:
             self.child_id_lists = []
             for sub_rule_string in content.split('|'):
-                sub_rule_ids = list(map(lambda x: int(x), sub_rule_string.strip().split(' ')))
+                sub_rule_ids = [int(x) for x in sub_rule_string.strip().split(' ')]
                 self.child_id_lists.append(sub_rule_ids)
-            # print(self.child_id_lists)
+
+    def __repr__(self):
+        if self.char != None:
+            return f"{self.id}: \"{self.char}\""
+        if len(self.child_id_lists) > 0:
+            child_id_str = ' | '.join([' '.join([str(y) for y in x]) for x in self.child_id_lists])
+            return f"{self.id}: {child_id_str}"
+        else:
+            raise Exception(f'The Rule {self.id} is malformed')
 
 
 def parse_rules(lines):
@@ -43,23 +54,21 @@ def parse_messages(lines):
             messages.append(line)
     return messages
 
-def generate_valid_messages(node, loop_track={}):
-    # print(f'Node {node.id}')
-    loop_track[node.id] = loop_track.get(node.id, 0) + 1
-    if loop_track.get(8, 0) > LOOP_LIMIT or loop_track.get(11, 0) > LOOP_LIMIT:
-        print(f'Hit limit for {node.id} ({loop_track.get(node.id)})')
-        return set()
+
+def generate_valid_messages(node):
     if node.char != None:
         return set([node.char])
     else:
         messages = set()
-        # print(node.child_id_lists)
         for id_list in node.child_id_lists:
             existing_messages = set()
-            for child_node in map(lambda id: node_map[id], id_list):
-                needs_copy = 8 in loop_track.keys() or 11 in loop_track.keys()
-                new_loop_track = loop_track.copy() if needs_copy else {}
-                new_messages = generate_valid_messages(child_node, new_loop_track)
+            for child_node in map(lambda id: rule_map[id], id_list):
+
+                if child_node in result_map.keys():
+                    new_messages = result_map.get(child_node)
+                else:
+                    new_messages = generate_valid_messages(child_node)
+
                 if len(existing_messages) == 0:
                     existing_messages = new_messages
                 else:
@@ -72,14 +81,36 @@ def generate_valid_messages(node, loop_track={}):
         return messages
 
 
-def get_parent_ids(rules, node_map):
-    all_ids = set(node_map.keys())
-    child_ids = set([id
-        for rule in rules
-        for id_list in rule.child_id_lists
-        for id in id_list
-    ])
-    return list(all_ids.difference(child_ids))
+# split into chunks of 8
+# 0 = 8 11
+# 8 = (42)+
+# 11 = 42 (42 31)* 31
+def is_valid_message(message, result_map):
+    chunks = [message[i*8:(i+1)*8] for i in range(len(message) // 8)]
+
+    start_valid = chunks[0] in result_map[42]
+    end_valid = chunks[-1] in result_map[31]
+    rest = chunks[1:]
+
+    cut = ceil(len(rest) / 2)
+
+    rest_1 = rest[:cut]
+    rest_2 = rest[cut:]
+
+    rest_1_valid = reduce(lambda a, b: a and b in result_map[42], rest_1, True)
+    rest_2_valid = True
+    hit_31 = False
+    for c in rest_2:
+        if c in result_map[42]:
+            if hit_31:
+                rest_2_valid = False
+        elif c in result_map[31]:
+            hit_31 = True
+        else:
+            rest_2_valid = False
+
+    valid = (start_valid and end_valid and rest_1_valid and rest_2_valid)
+    return valid
 
 
 f = open('input_2.txt')
@@ -93,28 +124,19 @@ print(f'Parsed {len(rules)} rules')
 print(f'Parsed {len(messages)} messages')
 
 for rule in rules:
-    node_map[rule.id] = rule
+    rule_map[rule.id] = rule
 
-parents = get_parent_ids(rules, node_map)
-print(f'Parent node(s): {parents}')
+m42 = generate_valid_messages(rule_map[42])
+m42_lens = set(map(lambda m: len(m), m42))
+print(f'Messages for Rule 42 ({len(m42)} possible): \n\tMax len: {max(m42_lens)}\n\tMin len {min(m42_lens)}')
+result_map[42] = m42
 
-
-# m42 = generate_valid_messages(node_map[42])
-# m42_lens = set(map(lambda m: len(m), m42))
-# print(f'Mssages for 42 ({len(m42)} possible): \n\tMax len: {max(m42_lens)}\n\tMin len {min(m42_lens)}')
-
-m31 = generate_valid_messages(node_map[31])
+m31 = generate_valid_messages(rule_map[31])
 m31_lens = set(map(lambda m: len(m), m31))
-print(f'Mssages for 31 ({len(m31)} possible): \n\tMax len: {max(m31_lens)}\n\tMin len {min(m31_lens)}')
+print(f'Messages for Rule 31 ({len(m31)} possible): \n\tMax len: {max(m31_lens)}\n\tMin len {min(m31_lens)}')
+result_map[31] = m31
 
+print(f'Saved results: {list(result_map.keys())}')
 
-m11 = generate_valid_messages(node_map[11])
-m11_lens = set(map(lambda m: len(m), m11))
-print(f'Mssages for 11 ({len(m11)} possible): \n\tMax len: {max(m11_lens)}\n\tMin len {min(m11_lens)}')
-
-
-
-# possible_messages = generate_valid_messages(node_map[parents[0]], {})
-# print(f'{len(possible_messages)} possible messages')
-# valid_messages = list(filter(lambda m : m in possible_messages, messages))
-# print(f'Valid Messages: {len(valid_messages)} of {len(messages)}')
+valid_messages = list(filter(lambda m : is_valid_message(m, result_map), messages))
+print(f'Valid Messages: {len(valid_messages)} of {len(messages)}')
